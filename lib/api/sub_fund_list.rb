39c07c00965e31917dfe0a3392897bd699b212ca
Taylor Brooks <tbrooks@gmail.com>
2014-04-02 23:06:03 -0500

    added support for sub funds

---
+++ lib/api/sub_fund_list.rb
@@ -0,0 +1,55 @@
+module FellowshipOne
+
+  class SubFundList
+
+    include Enumerable
+
+    attr_reader :count, :page_number, :total_records, :additional_pages
+
+
+    # Constructor.
+    #
+    # @param options A hash of options for loading the list.
+    #
+    # Get the list of sub-funds for a given fund.
+    #
+    # Options:
+    # :reader - (optional) The Reader to use to load the data.
+    def initialize(fund_id, options = {})
+      reader = options[:reader] || FellowshipOne::SubFundListReader.new(fund_id, options)
+      @json_data = reader.load_feed
+      @count = @json_data['subFunds']['subFund'].size.to_i
+      @page_number = 1
+      @total_records = @count
+      @additional_pages = 0
+    end
+
+    # Get the specified fund.
+    #
+    # @param index The index of the fund to get.
+    #
+    # @return Fund
+    def [](index)
+      Fund.new( @json_data['funds']['fund'][index] ) if @json_data['funds']['fund'][index]
+    end
+
+
+    # This method is needed for Enumerable.
+    def each &block
+      @json_data['subFunds']['subFund'].each{ |cont_recpt| yield( Fund.new(cont_recpt) )}
+    end
+
+    # Alias the count method
+    alias :size :count
+
+
+    # Checks if the list is empty.
+    #
+    # @return True on empty, false otherwise.
+    def empty?
+      self.count == 0 ? true : false
+    end
+
+  end
+  
+end
